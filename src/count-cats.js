da8ba58dac33246c92cc73f4b6ae4ccf47c6e684
LudmilaSokol <sluda999@yandex.ru>
2023-12-13 16:55:34 +0500

    init: start the BasicJS task

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,8 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let sum = 0;
+  for (let elem of matrix) {
+    let countCatsInElem = 0;
+    countCatsInElem = elem.filter(item => item ==='^^').length;
+    sum = sum + countCatsInElem;
+  }
+  return sum;
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
